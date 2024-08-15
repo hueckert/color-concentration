edb7877458184675fa8635149232d06eb1c4699c
hueckert <howard.ueckert@protonmail.com>
2024-08-15 15:26:24 -0500

    add psudo code to js file

---
+++ app.js
@@ -0,0 +1,28 @@
+//1) Define the required variables used to track the state of the game.
+
+//2) Store cached element references.
+
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+
+//4) The state of the game should be rendered to the user.
+
+//5) Define the required constants.
+
+//6) Handle a player clicking a square with a function to turn over the tile and remain turned over.
+
+//6a) Handle a player clicking a square with a function to turn over next tile and determine if colors match.
+
+//6b) if colors match remain turned over. go back up to step 6 and repeat. 
+
+//7) State a win when all color tiles have been matched and reamin turned over. 
+
+/*-------------------------------- Constants --------------------------------*/
+
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
